76de8f1bd6f836ef3f46424b318ae1c5ca55c014
hellowsj <15848729014@163.com>
2024-12-28 22:36:27 +0800

    first commit

---
+++ hm_src/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define HAVE_DECL_GETTID 1
+#define UDEV_HWDB_BIN "/data/service/el1/public/udev/hwdb.bin"
+#define UDEV_HWDB_DIR "/data/service/el1/public/udev/hwdb.d"
+#define UDEV_CONF_FILE "/etc/udev/udev.conf"
+#define UDEV_CONF_DIR "/data/service/el1/public/udev"
+#define UDEV_RULES_DIR "/etc/udev/rules.d"
+#define UDEV_ROOT_RUN "/data/service/el1/public"
+#define UDEV_LIBEXEC_DIR "/data/service/el1/public/udev/"
+#define program_invocation_short_name "udev"
+#define UDEV_VERSION "243"
+#define VERSION "3.2.10"
+#define RLIMIT_NLIMITS 15
+#define RLIMIT_NLIMITS 15
+#define IPTOS_LOWCOST 0x02
+#define LINE_MAX 2048
